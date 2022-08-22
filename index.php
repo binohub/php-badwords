9fe9f3ea6c6f285ee5843e3f20d8ba73de27eeb6
binohub <yeebino001@gmail.com>
2022-08-22 14:45:25 +0200

    done without get

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     $age = "22";
     $spiderman = $nome . " " . $cognome . " " . $age;
     $paragrafoditesto = "lorem ipsum dolor, sit amet consectetur lorem adipisicing elit. Quas assumenda officiis nesciunt obcaecati et recusandae atque cum id saepe illo eum, voluptatibus asperiores ipsa. Aut commodi ducimus doloremque velit magnam.";
-    $nuovastringa = str_replace("lorem", "*****", $paragrafoditesto,)
+    $nuovastringa = str_replace("lorem", "***", $paragrafoditesto,)
 ?>
 
 <!DOCTYPE html>
@@ -23,16 +23,18 @@
             var_dump($cognome);
             var_dump($age);
             var_dump($spiderman);
-            var_dump("il paragrafo di testo possiede una length di:" . " " . strlen($paragrafoditesto) . " caratteri");
+
         ?>
     </h5>
     <h4>
         <?php
+            var_dump("il paragrafo di testo possiede una length di: " . strlen($paragrafoditesto) . " caratteri");
             echo $paragrafoditesto;
         ?>
     </h4>
     <h4>
         <?php
+            var_dump("la nuova stribga possiede una leength di: " . strlen($nuovastringa) . "caratteri");
             echo $nuovastringa;
         ?>
     </h4>
